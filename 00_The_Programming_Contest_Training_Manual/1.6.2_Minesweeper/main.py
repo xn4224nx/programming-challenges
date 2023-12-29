3f4c645655e96bf78d3be6b1a8fc282b38adbe25
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-12-29 14:46:32 +0000

    Boilerplate for minesweeper challenge.

---
+++ 00_The_Programming_Contest_Training_Manual/1.6.2_Minesweeper/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import numpy as np
+
+def create_minefield(size: [int, int], mine_prob: float) -> str:
+    pass
+
+def parse_minefield(minefield: str) -> np.array:
+    pass   
+    
+def mine_count(mines: np.array) -> str:
+    pass
