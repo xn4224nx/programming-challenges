85166dbe911e7bb775a4e649003fe3e3d74fb39e
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-01-06 17:10:43 +0000

    Parse a cost file.

--- 00_The_Programming_Contest_Training_Manual/1.6.3_The_Trip/src/main.rs
+++ 00_The_Programming_Contest_Training_Manual/1.6.3_The_Trip/src/main.rs
@@ -1,3 +1,44 @@
+use std::fs;
+
+fn parse_trip_costs(filepath: &str) -> Vec<Vec<u32>> {
+
+    let contents = fs::read_to_string(String::from(filepath)).unwrap();
+
+    /* Storage for the trip costs. */
+    let mut costs: Vec<u32> = Vec::new();
+    let mut all_costs: Vec<Vec<u32>> = Vec::new();
+
+    /* Iterate over the string and consume it. */
+    for line in contents.lines() {
+        /* The file ends with a "0" */
+        if line == "0" {
+            break;
+
+        /* Detect the start of a new trip. */
+        } else if !line.contains(".") {
+            /* Save the costs. */
+            if !costs.is_empty() {
+                all_costs.push(costs.clone());
+                costs.clear();
+            }
+
+            /* Detect the number of pupils on the trip. */
+            let students: usize = line.parse().unwrap();
+
+            /* Allocate space for an array of travel costs. */
+            costs.reserve(students);
+        } else {
+            /* Otherwise save the amount to an array. */
+            let raw_cost: u32 = line.replace(".", "").parse().unwrap();
+            costs.push(raw_cost);
+        }
+    }
+
+    return all_costs;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let all_tripcosts = parse_trip_costs("./data/ques_00.txt");
+
+    println!("{:?}", all_tripcosts);
 }
