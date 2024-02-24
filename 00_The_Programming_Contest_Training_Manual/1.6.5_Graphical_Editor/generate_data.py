48a3e6719b80aeeef304994f034330a5853e9574
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-02-24 17:35:37 +0000

    Boilerplate challenge files.

---
+++ 00_The_Programming_Contest_Training_Manual/1.6.5_Graphical_Editor/generate_data.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+GENERATE GRAPHICAL EDITOR SAMPLES
+=================================
+
+Create a number of random graphical editor sample files and the final 
+output they would create.
+"""
